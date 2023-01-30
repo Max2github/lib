10d8c9175ea5331b3e7246eec30994eaf4d91351
Max2github <Your@email>
2023-01-30 11:22:47 +0100

    comment for words_template

--- templates/words_template.h
+++ templates/words_template.h
@@ -1,3 +1,25 @@
+/**
+ * @file words_template.h
+ * @author Max2
+ * @brief functions originating from words.h as macros
+ * The following macros are based on well-working functions
+ * of words.h in order to use their concept more generally.
+ * It should be sort of like a template in C.
+ * These macros should not be used directly,
+ * but should be called in an own function.
+ * 
+ * - FIND_TEMPLATE - general find function
+ *   Originaly for arrays, but can also be used with other formats / types / structures.
+ *   + e.g. FIND_TEMPLATE(
+ *       list1, list2, // the lists - seach (entire) list2 in list1
+ *       list_len, list_len, // a function (or clever (!) macro) for getting the number of elements in each list
+ *       int, // the type which is returned from the lenth functions
+ *       list_getIndex(i), list_getIndex(n) // get the individual elements
+ *     )
+ *   + Warning: This macro may not always be efficient, especially not for linked lists.
+ * 
+ */
+
 #ifndef WORDS_TEMPLATE_H
 #define WORDS_TEMPLATE_H
 
