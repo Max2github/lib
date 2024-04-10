2d3aca4dbb938e8da667d0b0c8a127ec8baae556
Max2 <38298972+Max2github@users.noreply.github.com>
2024-04-10 22:21:28 +0200

    use min warmup time again + test
    Used min warmup time again in order to stabilize results,
    but they keep being strange.
    
    The Minibuffer is very slow on windows and too fast on linux.
    The first iteration of BM_SmartBuffer is too slow on Windows,
    it simply cannot be that appending 2 x 4000 bytes is faster
    than appending 1 x 4000 bytes (and with a factor ~4).
    This only happened on Windows VM ARM64 though...
    Not even on Windows VM x64...

--- test/benchmark/smart/string_append_cont.cpp
+++ test/benchmark/smart/string_append_cont.cpp
@@ -8,7 +8,7 @@ const char * str = strToAdd.c_str();
 
 void SetBenchmarkParams(benchmark::internal::Benchmark * benchmark) {
     benchmark
-        //->MinWarmUpTime(std::chrono::seconds(5).count())
+        ->MinWarmUpTime(std::chrono::seconds(5).count())
         //->Iterations(1000)
         //->RangeMultiplier(2)->Range(1<<10, 1<<18)
         ->RangeMultiplier(2)->Range(1, 1<<10)
