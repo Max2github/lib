c17812bfce38b9bb52feffe90684eacd6c419f6e
Max2 <38298972+Max2github@users.noreply.github.com>
2024-04-12 09:25:17 +0200

    "norealloc" option
    Introduced a "norealloc" option, that enables
    that the Minibuffer does not use realloc(),
    but an own "no_realloc" that just does:
    
    1. alloc new
    2. copy old to new
    3. free old
    
    The results prove my hypothesis from before,
    that the Minibuffer was so fast because it used
    the standard C function realloc,
    which is apparently well implemented in Glibc (Ubuntu)
    and in Apple LLVM (MacOs), but NOT well implemented
    on Windows (clang - same libraries as MSVC).

--- test/benchmark/smart/string_include.hpp
+++ test/benchmark/smart/string_include.hpp
@@ -1,6 +1,33 @@
 #include "../../../CPP/smartstring/smartstring.hpp"
 #include "../../../CPP/smartbuffer/smartbuffer.hpp"
 
+#define NO_REALLOC 1
+
+#ifdef USE_CHAR_TRAITS
+
+#include <string>
+//#define SIMPLE_ARRAY_MEMCOPY(dest, src, size) __builtin_memcpy((void *) (dest), (const void *) (src), (unsigned long long) (size))
+#define SIMPLE_ARRAY_MEMCOPY(dest, src, size) std::char_traits<char>::copy((char *)dest, (const char *)src, size)
+
+#endif
+
+#ifdef NO_REALLOC
+
+#undef INCLUDE_C_SOURCES
+#define INCLUDE_C_SOURCES 1
+
+void * no_realloc(void* oldPtr, const size_t newsize, const size_t oldSize) {
+    void * res = SMARTSTRING_H_MALLOC(newsize);
+    SIMPLE_ARRAY_MEMCOPY(res, oldPtr, oldSize);
+    SMARTSTRING_H_FREE(oldPtr);
+    return res;
+}
+
+#undef SMARTSTRING_H_REALLOC
+#define SMARTSTRING_H_REALLOC(oldPtr, newsize, oldSize) no_realloc(oldPtr, newsize, oldSize)
+
+#endif
+
 #define INCLUDE_CPP_SOURCES 1
 
 #ifdef INCLUDE_C_SOURCES
