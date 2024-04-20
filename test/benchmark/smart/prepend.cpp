1ff9d1aceef825031f8e34e638c3be300c09673b
Max2 <38298972+Max2github@users.noreply.github.com>
2024-04-20 12:04:18 +0200

    prepend more iterations

--- test/benchmark/smart/prepend.cpp
+++ test/benchmark/smart/prepend.cpp
@@ -17,7 +17,7 @@
 void SetBenchmarkParams(benchmark::internal::Benchmark * benchmark) {
     benchmark
         ->MinWarmUpTime(std::chrono::seconds(5).count())
-        ->RangeMultiplier(2)->Range(1<<10, 1<<18)
+        ->RangeMultiplier(2)->Range(1<<10, 1<<20)
         //->RangeMultiplier(2)->Range(1, 1<<10)
         ->Complexity(benchmark::BigO::oAuto)
     ;
